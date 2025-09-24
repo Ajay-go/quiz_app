e6432d3c70108dfcb6f1546246308b87027ee28c
ajay gour <ajaygour3333@gmail.com>
2025-09-24 21:41:18 +0530

    complete backend

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,25 @@
-const express = require("express")
-// import 'dotenv/config';
-// import cors from 'cors';
+require('dotenv').config();
+const express = require("express");
 const cors = require("cors");
-const connectMongoDb = require("./connection")
-const {userRouter} = require("./routes/user.routes");
-const {paperRouter} = require("./routes/paper.routes")
+const connectMongoDb = require("./connection");
+const { userRouter } = require("./routes/user.routes");
+const { paperRouter } = require("./routes/paper.routes");
 
-
-const app = express();
+const mongo_url = process.env.MONGO_URL;
 const port = process.env.PORT || 4000;
 
-connectMongoDb("mongodb://127.0.0.1:27017/quizapp").then(() => console.log("Mongodb connected")).catch(() => console.log("MongoDb disconnected"));
+const app = express();
 
-
-// middlewares
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(express.urlencoded({extended: true}));
 
 app.use("/", userRouter);
 app.use("/quiz", paperRouter);
 
-
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
+connectMongoDb(mongo_url)
+    .then(() => {
+        console.log("MongoDB connected");
+        app.listen(port, () => console.log(`Server running on port ${port}`));
+    })
+    .catch(err => console.error(" MongoDB connection error:", err));
