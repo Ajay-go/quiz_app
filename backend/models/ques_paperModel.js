fcb42f6bcbc9f757677008675f0c3fca2804f88d
ajay gour <ajaygour3333@gmail.com>
2025-09-25 11:07:36 +0530

    change in question model

--- backend/models/ques_paperModel.js
+++ backend/models/ques_paperModel.js
@@ -1,42 +1,30 @@
 const mongoose = require("mongoose");
 
-const paper_Schema = new mongoose.Schema({
-    paper_id :{
-        type : Number,
-        unique : true,
-        required : true,
+const paperSchema = new mongoose.Schema({
+    paper_id: {
+        type: Number,
+        unique: true,
+        required: true,
     },
     teacher_id: {
         type: String,
-        required: true
+        required: true,
     },
-    questionsAndAnswers : [
-        {
-            question_text: {
-                type: String, 
-                unique: true,
-            }, 
-            options: [
-                {
-                    option_id : {
-                    type: String,
-                    required: true
-                    },
-                    option_text: {
-                        type: String,
-                        required: true
-                    }
-                }
-            ],
-            answer: {
-                type: String,
-                required: true
-            }
-        }
-    ]
-})
+    questions: {
+        type: [String], // array of question strings
+        required: true,
+    },
+    answers: {
+        type: [String], // array of correct answers
+        required: true,
+    },
+    options: {
+        type: Map,
+        of: [String], // each key (0,1,2,3) maps to an array of options
+        required: true,
+    },
+}, { timestamps: true });
 
-const Paper = mongoose.model("Paper",paper_Schema);
-
+const Paper = mongoose.model("Paper", paperSchema);
 
 module.exports = Paper;
